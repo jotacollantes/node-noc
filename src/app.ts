9eda445cc29a7d3b87180b47034c8cae9d9890af
Juan Collantes <juan@collantes.ec>
2023-12-08 20:28:07 -0500

    comentarios add

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Server } from './presentation/server';
 
-
+//El punto de arranque de la aplicacion es el app.ts que ejecuta una funcion anonima auto ejecutable que esta a su vez ejecuta el metodo estatico start() sin instanciar la clase Server que esta en la capa de presentacion
 (async() => {
   main();
 })();
